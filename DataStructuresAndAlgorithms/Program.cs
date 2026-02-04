9c1758b275582e7a4e080386bb4e636138e4fb56
Oleksandr Kovalchuk <oleksandr.kovalchuk@avenga.com>
2026-02-04 17:25:49 +0100

    add merge method for future merge sort

--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -14,6 +14,7 @@ class Program
         // LongestCommonPrefix.RunTests();
         // RemoveDuplicatesFromSortedArray.RunTests();
         // RemoveElement.RunTests();
-        BubbleSort.RunTest();
+        // BubbleSort.RunTest();
+        MergeSort.RunTest();
     }
 }
---
+++ DataStructuresAndAlgorithms/Sorting/MergeSort.cs
@@ -0,0 +1,64 @@
+using DataStructuresAndAlgorithms.Helpers;
+
+namespace DataStructuresAndAlgorithms.Sorting;
+
+public class MergeSort
+{
+    public static int[] MergeTwoSubArrays(int[] arr1, int[] arr2)
+    {
+        int n1 = arr1.Length;
+        int n2 = arr2.Length;
+        int i = 0, j = 0;
+        int[] result = new int[n1+n2];
+        int k = 0;
+
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+                
+            }
+            else {
+                result[k] = arr2[j];
+                j++;
+                
+            }
+            k++;
+        }
+        
+        if (i < n1)
+        {
+            while (i < n1)
+            {
+                result[k] = arr1[i];
+                i++;
+                k++;
+            }
+            
+        }
+        else if (j < n2)
+        {
+            while (j < n2)
+            {
+                result[k] =  arr2[j];
+                j++;
+                k++;
+            }
+        }
+        
+        return result;
+    }
+
+    public static void RunTest()
+    {
+        int[] arr1 = [3, 19];
+        int[] arr2 = [2, 11];
+        int[] result = MergeTwoSubArrays(arr1, arr2);
+        ArrayHelper.PrintArray(result, "Merge two sub arrays: [3, 19] && [2, 11]");
+        
+        int[] arr3 = [3, 19, 28];
+        int[] arr4 = [2, 11, 13, 15];
+        int[] result2 = MergeTwoSubArrays(arr3, arr4);
+        ArrayHelper.PrintArray(result2, "Merge two sub arrays: [3, 19, 28] && [2, 11, 13, 15]: ");
+    }
+}
